899b086fedf8c0eccc2fefa90bb1f7624dc98c9c
hyperion2144 <2840956925@qq.com>
2026-07-11 21:36:05 +0800

    chore(commands): wire registerResumeCommand into commands router(commands)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,6 +2,7 @@ import type { CAC } from 'cac'
 import { registerConfigCommand } from './config.ts'
 import { registerInitCommand } from './init.ts'
 import { registerProfileCommand } from './profile.ts'
+import { registerResumeCommand } from './resume.ts'
 
 /**
  * Register top-level subcommands on the cac root program.
@@ -10,4 +11,5 @@ export function registerCommands(program: CAC): void {
   registerInitCommand(program)
   registerConfigCommand(program)
   registerProfileCommand(program)
+  registerResumeCommand(program)
 }
